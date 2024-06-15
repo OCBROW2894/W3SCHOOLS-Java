2bf5012a9d6c2f4496a6473a4df9423019236e14
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-06-15 23:14:54 +0300

    OUTPUT

---
+++ OutPut.java
@@ -0,0 +1,13 @@
+
+public class OutPut{
+	public static void main (String[] Args){
+		System.out.print("OCBROW_");
+		System.out.print(2894);
+		System.out.println("mathematical Calculations");
+		System.out.println(2800+94);
+		System.out.println(1247*2);
+		System.out.println(5788/2);
+		System.out.println(2894%2);
+		
+	}
+}
