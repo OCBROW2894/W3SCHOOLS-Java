d22dad9b1ecbcc9627337a035af4fa825445ed1e
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-06-15 23:42:12 +0300

    Comments

---
+++ Comments.java
@@ -0,0 +1,17 @@
+
+/*This program is just meant to show the different types of comments
+ *In Java SINGLE LINE & MULTI LINE
+ */
+ 
+ public class Comments{
+	 public static void main(String[] Args ){
+		 System.out.print("Bryian "); //My First Name
+		 System.out.print("Owuor "); //My Middle Name
+		 System.out.print("Ochieng"); //My Last Name
+		 /*I used The Multi-line Comment 
+		 at the start to tell U what the program is about this really
+		 helps one to know & understand all the functions in the program
+		 NOTE: Really comes in Handy when the program has so many lines of code
+		 */
+	 }
+ }
